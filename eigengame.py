612bf6a251f25a5b5af451d4ced1fcc2d65a3d44
RishanthRajendhran <rishanthrajendhran@outlook.com>
2021-08-09 08:17:46 +0530

    Finished the basic game

---
+++ eigengame.py
@@ -0,0 +1,86 @@
+import sys
+import numpy as np
+
+xDim = (100000, 10)
+numIterations = 10000
+k = 10
+
+#Function to return the reward
+#Inputs
+#   X   - Numpy array of the dataset
+#   V   - Numpy array whose columns should eventually represent the eigenbectors of X
+#   i   - Index of eign vector in consideration
+#Outputs
+#   Returns a vector reward of dimension X.shape[0] x 1
+def getReward(X, V, i):
+    return (np.dot(X, V[:,i])).reshape(-1, 1)
+
+#Function to return the penalty
+#Inputs
+#   X   - Numpy array of the dataset
+#   V   - Numpy array whose columns should eventually represent the eigenbectors of X
+#   i   - Index of eign vector in consideration
+#Outputs
+#   Returns a vector penalty of dimension X.shape[0] x 1
+def getPenalty(X, V, i):
+    M  = np.dot(X.T, X)
+    penalty = np.zeros((X.shape[0], 1))
+    for j in range(X.shape[1]):
+        if j != i:
+            penalty += (np.dot(np.dot(X, V[:,i]), np.dot(X, V[:,j]))/np.dot(np.dot(X, V[:,j]), np.dot(X, V[:,j])))*np.dot(X,V[:,j]).reshape(-1,1)
+    return penalty.reshape(-1, 1)
+
+#Function to return the penalty
+#Inputs
+#   X       - Numpy array of the dataset
+#   reward  - Reward term
+#   penalty - Penalty term
+#Outputs
+#   Returns a vector gradient of dimension X.shape[1]  x 1
+def getGradUtility(X, reward, penalty):
+    return 2*np.dot(X.T,(reward - penalty))
+
+#Function to update eigenvectors 
+#Inputs 
+#   X       - Numpy array of the dataset
+#   V       - Numpy array whose columns should eventually represent the eigenbectors of X
+#   i       - Index of eign vector in consideration
+#   reward  - Reward term
+#   penalty - Penalty term
+#   alpha   - step size of updates
+#Outputs
+#   Returns updated numpy array of eigenvectors 
+def updateEigenVectors(X, V, i, reward, penalty, alpha=0.00005):
+    gradV = getGradUtility(X, reward, penalty)
+    gradV = gradV - np.dot(gradV.T, V[:,i])*V[:,i].reshape(gradV.shape[0],1)
+    V[:,i] = V[:,i] + alpha*gradV.reshape(gradV.shape[0],)
+    V[:,i] = V[:,i]/np.sqrt(np.sum(V[:,i]**2))
+    return V
+
+#Function to find eigenvectors of a given X
+#Inputs 
+#   X   - Numpy array of the dataset
+#   T   - Number of iterations for each player 
+#   k   - Number of eigenvectors to find
+#Outputs
+#   Returns k eigenvectors of V as a matrix of dimensions X.shape[1] x k
+def playEigenGame(X, T, k):
+    X = np.array(X)
+    V = np.ones((X.shape[1],k))
+    for i in range(k):
+        for t in range(T):
+            reward = getReward(X, V, i)
+            penalty = getPenalty(X, V, i)
+            V = updateEigenVectors(X, V, i, reward, penalty)
+    return V
+
+if "-generateX" in sys.argv:
+    X = np.random.rand(xDim[0], xDim[1])
+    np.save("./X.npy",X)
+X = np.load("./X.npy")
+print("Playing the EigenGame...")
+V = playEigenGame(X, numIterations, k)
+print("EigenVectors obtained through EigenGame:")
+print(np.around(V,decimals=3))
+print("\nEigenVectors obtained through numpy:")
+print(np.around(np.linalg.eig(np.dot(X.T, X))[1][:,:k],decimals=3))
