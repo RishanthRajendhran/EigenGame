fb8100485571ec06155a912203aebc6f2455f8c0
RishanthRajendhran <rishanthrajendhran@outlook.com>
2021-08-25 23:53:29 +0530

    Added flags, associated checking and analyseResults

--- eigengame.py
+++ eigengame.py
@@ -1,10 +1,52 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import os.path
 
 xDim = (100000, 4)
-numIterations = 100000
+numIterations = 10000
 k = 4
+learningRate = 0.000011
+
+#Function to return the distance measure 
+# between current player positions (current eigenvalues) 
+# and expected final player positions (i.e. expected eigenvalues)
+#Inputs
+#   V   - Current player positions
+#   EVs - Expected final player positions
+#Outputs
+#   Returns a scalar distance measure
+def getDistance(V, EVs):
+    return np.sqrt(np.sum((V - EVs) ** 2))      #Euclidean distance
+
+#Function to rearrange columns of matrix A based on their closest matching column index in matrix B
+#Inputs 
+#   A   - Matrix whose columns have to be rearranged
+#   B   - Matrix which is the standard based on which rearrangements have to be done
+#Outputs
+#   Returns rearranged A matrix
+def rearrange(A, B):
+    newA = A.copy()
+    for i in range(A.shape[1]):
+        a = A[:,i]
+        minDist = np.inf 
+        minCol = i 
+        isNeg = False
+        for j in range(B.shape[1]):
+            b = np.around(B[:,j],decimals=3).copy()
+            dist = getDistance(a, b)
+            distNeg = getDistance(a, -b)
+            if dist < distNeg and dist < minDist:
+                minDist = dist 
+                minCol = j
+                isNeg = False
+            elif distNeg < minDist:
+                minDist = distNeg 
+                minCol = j
+                isNeg = True
+        newA[:,minCol] = (not isNeg)*A[:,i].copy() - isNeg*A[:,i].copy()
+    return newA
 
 #Function to return the reward
 #Inputs
@@ -27,7 +69,10 @@ def getPenalty(X, V, i):
     M  = np.dot(X.T, X)
     penalty = np.zeros((X.shape[0], 1))
     for j in range(X.shape[1]):
-        if j < i:
+        condition = j < i
+        if "-modified" in sys.argv:
+            condition = j != i
+        if condition:
             penalty += (np.dot(np.dot(X, V[:,i]), np.dot(X, V[:,j]))/np.dot(np.dot(X, V[:,j]), np.dot(X, V[:,j])))*np.dot(X,V[:,j]).reshape(-1,1)
     return penalty.reshape(-1, 1)
 
@@ -51,12 +96,21 @@ def getGradUtility(X, reward, penalty):
 #   alpha   - step size of updates
 #Outputs
 #   Returns updated numpy array of eigenvectors 
-def updateEigenVectors(X, V, i, reward, penalty, alpha=0.00000075):
+def updateEigenVectors(X, V, i, reward, penalty, alpha=learningRate):
     gradV = getGradUtility(X, reward, penalty)
     gradV = gradV - np.dot(gradV.T, V[:,i])*V[:,i].reshape(gradV.shape[0],1)
     V[:,i] = V[:,i] + alpha*gradV.reshape(gradV.shape[0],)
     V[:,i] = V[:,i]/np.sqrt(np.sum(V[:,i]**2))
     return V
+
+#Function to find eigenvectors of a given X using Numpy
+#Inputs 
+#   X   - Numpy array of the dataset
+#   k   - Number of eigenvectors to find
+#Outputs
+#   Returns k eigenvectors of V as a matrix of dimensions X.shape[1] x k
+def getEigenVectorsK(X, k=1):
+    return np.linalg.eig(np.dot(X.T, X))[1][:,:k]
 
 #Function to find eigenvectors of a given X
 #Inputs 
@@ -67,45 +121,114 @@ def updateEigenVectors(X, V, i, reward, penalty, alpha=0.00000075):
 #   Returns k eigenvectors of V as a matrix of dimensions X.shape[1] x k
 def playEigenGame(X, T, k):
     X = np.array(X)
-    V = np.ones((X.shape[1],k))
+    V = None
+    if "-continueEigenGame" in sys.argv:
+        print("Continuing the last game...")
+        if "-modified" in sys.argv:
+            if os.path.exists("Vs_modified.npy") and os.path.isfile("Vs_modified.npy"):
+                V = np.load("Vs_modified.npy")[-1]
+            else:
+                print("Last game not found!\nStarting new game...")
+                V = np.ones((X.shape[1],k))
+        else: 
+            if os.path.exists("Vs.npy") and os.path.isfile("Vs.npy"):
+                V = np.load("Vs.npy")[-1]
+            else:
+                print("Last game not found!\nStarting new game...")
+                V = np.ones((X.shape[1],k))
+    if "-continueEigenGame" not in sys.argv or V.shape != (X.shape[1], k):
+        V = np.ones((X.shape[1],k))
     Vs = [V.copy()]
-    for i in range(k):
-        for t in range(T):
+    iterTimes = [0]      #Array to store time taken for every iteration
+    iterTimesSum = 0    #Variable to keep track of total time elapsed
+    for t in range(T):
+        startIter = time.time()
+        for i in range(k):
             reward = getReward(X, V, i)
             penalty = getPenalty(X, V, i)
             V = updateEigenVectors(X, V, i, reward, penalty)
-            Vs.append(V.copy())
+        Vs.append(V.copy())
+        stopIter = time.time()
+        timeIter = stopIter - startIter
+        iterTimesSum += timeIter
+        iterTimes.append(iterTimesSum)
+        if "-debug" in sys.argv and not t%100:
+            print(f"{t}/{T} => total time elapsed : {np.around(iterTimesSum,decimals=3)}s")
     Vs = np.array(Vs)
-    np.save("Vs_modified.npy",Vs)
+    iterTimes = np.array(iterTimes)
+    if "-continueEigenGame" in sys.argv:
+        if "-modified" in sys.argv:
+            oldVs = np.load("Vs_modified.npy")
+            oldIterTimes = np.load("iterTimes_modified.npy")
+            Vs = np.append(oldVs, Vs.copy(),0)
+            iterTimes = iterTimes + np.sum(oldIterTimes)
+            iterTimes = np.append(oldIterTimes, iterTimes.copy(),0)
+        else:
+            oldVs = np.load("Vs.npy")
+            oldIterTimes = np.load("iterTimes.npy")
+            Vs = np.append(oldVs, Vs.copy(),0)
+            iterTimes = iterTimes + np.sum(oldIterTimes)
+            iterTimes = np.append(oldIterTimes, iterTimes.copy(),0)
+    if "-modified" in sys.argv:
+        np.save("Vs_modified.npy",Vs)
+        np.save("iterTimes_modified.npy",iterTimes)
+    else:
+        np.save("Vs.npy",Vs)
+        np.save("iterTimes.npy",iterTimes)
     return V
 
-if "-generateX" in sys.argv:
-    X = np.random.rand(xDim[0], xDim[1])
-    np.save("./X.npy",X)
+#-------------------------------------------------------------------------------------------------------------------------
+
+if "-continueEigenGame" not in sys.argv:
+    if "-repeatedEVtest" in sys.argv:
+        X = [[-5,-6,3],[3,4,-3],[0,0,-2]]
+        X = np.array(X)
+    elif "-generateX" in sys.argv or not (os.path.exists("./X.npy") and os.path.isfile("./X.npy")):
+        X = np.random.rand(xDim[0], xDim[1])
+        np.save("./X.npy",X)
+elif not (os.path.exists("./X.npy") and os.path.isfile("./X.npy")):
+    print("Last game's dataset not found!\nStarting new game with new dataset...")
+
+#Load dataset X from "./X.npy"
 X = np.load("./X.npy")
-# X = [[-5,-6,3],[3,4,-3],[0,0,-2]]
-# X = np.array(X)
-# print(X)
-if "-analyseResults" in sys.argv:
-    Vs_modified = np.load("Vs_modified.npy")
-    EVs = np.around(np.linalg.eig(np.dot(X.T, X))[1][:,:k],decimals=3)
-    EVs = -EVs
-    temp = EVs[:,2].copy()
-    EVs[:,2] = EVs[:,3].copy() 
-    EVs[:,3] = -temp.copy()
-    print(EVs)
-    diffs = []
-    for V in Vs_modified:
-        dist = np.sqrt(np.sum((V - EVs) ** 2))
-        diffs.append(dist)
-        print(dist)
-    plt.plot(diffs)
-    plt.show()
-    print(Vs_modified.shape)
-else:
+
+if "-printX" in sys.argv:
+    print(X)
+
+if "-analyseResults" not in sys.argv or "-playEigenGame" in sys.argv:
     print("Playing the EigenGame...")
     V = playEigenGame(X, numIterations, k)
+    EVs = getEigenVectorsK(X, k)
+    V = rearrange(V, EVs)
     print("EigenVectors obtained through EigenGame:")
     print(np.around(V,decimals=3))
     print("\nEigenVectors obtained through numpy:")
-    print(np.around(np.linalg.eig(np.dot(X.T, X))[1][:,:k],decimals=3))
+    print(np.around(EVs,decimals=3))
+
+if "-analyseResults" in sys.argv:
+    if "-modified" in sys.argv:
+        Vs = np.load("Vs_modified.npy")
+        iterTimes = np.load("iterTimes_modified.npy")
+    else:
+        Vs = np.load("Vs.npy")
+        iterTimes = np.load("iterTimes.npy")
+    EVs = np.around(getEigenVectorsK(X, k),decimals=3)
+    EVs = rearrange(EVs, Vs[-1])
+    diffs = []
+    print("EigenVectors obtained through EigenGame:")
+    for V in Vs:
+        diffs.append(getDistance(V,EVs))
+        if "-debug" in sys.argv:
+            print(np.around(V,decimals=3))
+    if "debug" not in sys.argv:
+        print(np.around(Vs[-1],decimals=3))
+    print("\nEigenVectors obtained through numpy:")
+    print(np.around(EVs,decimals=3))
+    plt.plot(diffs)
+    plt.xlabel("Iterations")
+    plt.ylabel("Distance")
+    plt.show()
+    plt.plot(iterTimes, diffs)
+    plt.xlabel("Time elapsed (s)")
+    plt.ylabel("Distance")
+    plt.show()
--- readme.md
+++ readme.md
@@ -17,8 +17,21 @@
         Running the program
     </h4>
     Run the program with the flag "-generateX" if you want to generate X before playing the eigengame<br/>
-    The major hyperparameters of the game (dimensions of X, number of top eigenvectors to find, number of iterations to run) are declared at the top of the program right after the module imports<br/>
+    The "-printX" flag should be used to print the X being used in the eigenGame<br/>
+    The "-repeatedEVtest" flag should be used to use the hard-coded matrix X with repeated eigenvalues for the eigenGame<br/>
+    The major hyperparameters of the game (dimensions of X, number of top eigenvectors to find, number of iterations to run and the learning rate) are declared at the top of the program right after the module imports<br/>
     The program does not do any hyperparameter tuning<br/>
     It was observed that learning rate largely determined how close the solution obtained was to the actual eigenvectors<br/>
     Try experimenting with different powers of 10 for the learning rate<br/>
+    The program has to be run with the "-modified" flag to use the modified update<br/>
+    To continue the last played eigengame, use the "-continueEigenGame" flag<br/>
+    The program stores players' positions (read calculated eigenvectors) after every iteration of update in the file "Vs.npy" (or "Vs_modified.npy" when used in conjunction with the "-modified" flag)<br/>
+    The program stores total time elapsed since start of the game after every iteration of update in the file "iterTimes.npy" (or "iterTimes_modified.npy" when used in conjunction with the "-modified" flag)<br/>
+    Distance between the calculated eigenvectors after every iteration of update during the eigengame and the actual eigenvectors (obtained through numpy) vs the number of iterations and total time elapsed since the start of the game can be plotted by using the "-analyseResults" flag<br/>
+    When using the "-analyseResults" flag, if one wants to first play the eigengame, one has to also use the "-playEigenGame" flag while executing the program<br/> 
+    The "-debug" flag can be used to print some debug information<br/>
+    Arbitrary combination of flags might give undesirable results<br/>
+    For example, using the "-repeatedEVtest" flag  along with the "analyseResults" flag without the "-playEigenGame" flag will only work as expected if the last played eigen game was with the "-repeatedEVtest" flag<br/>
+    When run without flags, the program tries to get X from stored file in a hard-coded path. If not present, it would implicitly use the "-generateX" flag. The eigengame is then played and final results are printed<br/>
+    All labels are case sensitive<br/>
 </p>
